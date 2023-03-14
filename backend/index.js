edbddf3c99d25e9167e5baeea42865e7c5fa493c
AMAR DEEP <amar1998deep@gmail.com>
2023-03-14 23:11:43 +0530

    backend for chat completed

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,68 @@
 const express=require("express");
 const http=require("http");
+const {Server}=require("socket.io");
+const { SocketMiddleware }=require("./Middleware/Socket.Middleware");
+const { messageRouter } = require("./Router/messageRouter");
+const { userRouter } = require("./Router/userRouter");
+const { connection } = require("./Config/db");
+const cors=require("cors");
 require("dotenv").config();
 
+const app=express();
+
 const httpServer=http.createServer(app);
+
+const io=new Server(httpServer);
+
+app.use(express.json());
+app.use(cors({
+    origin:"*",
+}));
 
 
 app.get("/",(req,res)=>{
     res.send("Home");
 });
 
-httpServer.listen(process.env.port,()=>{
+let activeUsers=[];
+
+
+app.use("/user",userRouter);
+app.use("/message",messageRouter);
+
+app.use(SocketMiddleware);
+
+io.on("connection",(socket)=>{
+    socket.on("userID",(data)=>{
+        let check=activeUsers.filter((el)=>el.userID===data);
+        if(check.length===1){
+            activeUsers.forEach((el)=>{
+                if(el.userID===data){
+                    el.socketID=socket.id;
+                }
+            });
+        } else {
+            activeUsers.push({userID:data,socketID:socket.id});
+        }
+        socket.emit("activeUsers",activeUsers);
+    });
+    socket.on("message",(msg)=>{
+        let sendTo=activeUsers.filter((el)=>el.userID===msg.to);
+        socket.to(sendTo[0].socketID).emit("message",{messages:msg.message,from:msg.from});
+    });
+    socket.on("disconnect",(cc)=>{
+        activeUsers=activeUsers.filter((el)=>el.socketID!==socket.id);
+        socket.emit("activeUsers",activeUsers);
+    });
+});
+
+httpServer.listen(process.env.port,async()=>{
+    try{
+        await connection;
+        console.log("Connected to DB");
+    } catch(err){
+        console.log(err);
+        console.log("Not-Connected to DB");
+    }
     console.log(`server is running at port ${process.env.port}`);
 });
